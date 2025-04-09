182a2c96568504a466212b1ba00f1bc57fa57f4a
Felipe Castelar <felipecastelarvieira@gmail.com>
2025-04-09 16:19:25 -0300

    Add files via upload

--- Escola.java
+++ Escola.java
@@ -1,25 +1,26 @@
 public class Escola{
    private String nome;
+   private int Idade;
    private double nota;
    private static int contador = 0;
 
-    public Escola (String n,double Nota){
-        nome = n;
-        nota = Nota;
+    public Escola (String nome,double nota, int Idade) {
+        this.nome = nome;
+        this.nota = nota;
+        this.Idade = Idade;
         contador++;
-
     }
-
-
-
-    public void mostrarDados(){
+        public void mostrarDados(){
+        System.out.println("------------------------------");
         System.out.println("Nome: " +nome);
+        System.out.println("idade: "+Idade);
         System.out.println("Nota: " +nota);
         if (nota > 6 ) {
             System.out.println("Situação: Aprovado ✅");
         }else{
             System.out.println("Situação: Reprovado ❌");
         }
+        System.out.println("-------------------------------------");
         System.out.println();
     }
     public static void mostrarAluno(){
--- Main.java
+++ Main.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Escola a1 = new Escola("felipe",8.8);
-        Escola a2 = new Escola("Ana",5);
-        Escola a3 = new Escola("Nina",10);
 
-        a1.mostrarDados();
-        a2.mostrarDados();
-        a3.mostrarDados();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Escola>listaAlunos = new ArrayList<>();
+
+        System.out.println("quantos alunos vc quer cadastrar?");
+        int contadordeAlunos = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i< contadordeAlunos; i++){
+
+
+            System.out.println("digite o nome do aluno:");
+            String nome = scanner.nextLine();
+
+            System.out.println("digite a idade do aluno:");
+            int Idade = scanner.nextInt();
+
+            System.out.println("digite a nota dele:");
+            double nota = scanner.nextDouble();
+            scanner.nextLine();
+
+            Escola aluno = new Escola(nome, nota, Idade);
+            listaAlunos.add(aluno);
+
+        }
+        for (Escola aluno: listaAlunos){
+            aluno.mostrarDados();
+        }
 
         Escola.mostrarAluno();
     }
